3a86db94aa06e75fe8c6999dc830d587adf0f9c9
adanSmith01 <smith.paredes.13@gmail.com>
2025-01-06 21:21:17 -0300

    Add user model

---
+++ User.cs
@@ -0,0 +1,9 @@
+namespace UserAuthenticationJWT.Model;
+
+public class User
+{
+    public string FirstName { get; set;}
+    public string LastName { get; set;}
+    public int Id { get; set;}
+    public string Email { get; set;}
+}
