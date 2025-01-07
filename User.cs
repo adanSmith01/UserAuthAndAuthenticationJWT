a8181db58da0b502fac632a0c50e0705065a4756
adanSmith01 <smith.paredes.13@gmail.com>
2025-01-07 02:40:45 -0300

    File deleted

--- User.cs
+++
@@ -1,9 +0,0 @@
-namespace UserAuthenticationJWT.Model;
-
-public class User
-{
-    public string FirstName { get; set;}
-    public string LastName { get; set;}
-    public int Id { get; set;}
-    public string Email { get; set;}
-}
